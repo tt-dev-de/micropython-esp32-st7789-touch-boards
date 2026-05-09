3decf8461c1ea4d845b73ca81f2c30edb06f4e78
Thomas Tillig <tillig.th@gmail.com>
2026-05-09 16:34:32 +0200

    Update font_11x22_ter_u22b.py

--- src/fonts/font_11x22_ter_u22b.py
+++ src/fonts/font_11x22_ter_u22b.py
@@ -6,6 +6,7 @@ Source BDF: ter-u22b.bdf
 Source family: Terminus
 Source font: -xos4-Terminus-Bold-R-Normal--22-220-72-72-C-110-ISO10646-1
 Source copyright: Copyright (C) 2014 Dimitar Toshkov Zhekov
+Source Licensed under the SIL Open Font License, Version 1.1
 
 Format: fixed width, row-major bitmap, MSB left-aligned.
 Original BDF cell preserved: 11x22 pixels.
